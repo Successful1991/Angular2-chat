ab89b99b560a175d7723afbcb483268455fa6227
Tsys Artem <19successful91@gmail.com>
2018-10-04 17:57:18 +0300

    fix route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,7 @@ import {ChatDialogComponent} from "./chat/chat-dialog/chat-dialog.component";
 import {HomePageComponent} from "./home-page/home-page.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomePageComponent},
+  {path: '', component: HomePageComponent},
   {path: 'chat-dialog/:id' , component: ChatDialogComponent}
 ];
 
